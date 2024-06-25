b4d54c9bd75daf9215fa71c449820db826c9ff8b
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2024-06-25 14:55:43 +0100

    Use fixtures for unit test values

--- tests/python/test_values.py
+++ tests/python/test_values.py
@@ -8,29 +8,57 @@ Mock unit tests of functions which fail on certain values.
 """
 
 import math
+import pytest
 
 def my_floor(x):
   """Mock implementation of a "floor" function, which fails on negative non-integer values.
   """
   return int(x)
 
-def test_my_floor_pass():
+@pytest.fixture
+def test_values_pass():
+  return [0.5, 1.0, 0.0, 1.1, 2.3, 100000, 1e99]
+
+@pytest.fixture
+def test_values_fail():
+  return [0, 0.0, 1.0, 1.2, 1.9, -1.0, -1.5, -1.9, -2]
+
+@pytest.fixture
+def test_values_err():
+  return [0, 0.0, "0", "0.5"]
+
+@pytest.fixture
+def test_values():
+  return [0, 0.0, "0", "0.5"]
+
+def _test_my_floor(_test_values):
+  """Common implementation of tests.
+  """
+  for x in _test_values:
+    assert my_floor(x)==math.floor(x), f"my_floor failed for input {x}"
+
+def test_my_floor_pass(test_values_pass):
   """Mock unit test for `my_floor` which is expected to pass.
   """
+  _test_my_floor(test_values_pass)
 
-  for x in [0.5, 1.0, 0.0, 1.1, 2.3, 100000, 1e99]:
-    assert my_floor(x)==math.floor(x), f"my_floor failed for input {x}"
-
-def test_my_floor_fail():
+def test_my_floor_fail(test_values_fail):
   """Mock unit test for `my_floor` which is expected to fail.
   """
+  _test_my_floor(test_values_fail)
 
-  for x in [0, 0.0, 1.0, 1.2, 1.9, -1.0, -1.5, -1.9, -2]:
-    assert my_floor(x)==math.floor(x), f"my_floor failed for input {x}"
-
-def test_my_floor_err():
-  """Mock unit test for `my_floor` which is expected to raise an exception.
+def test_my_floor_err(test_values_err):
+  """Mock unit test for `my_floor` which is expected to raise an exception other than an AssertionError.
   """
+  _test_my_floor(test_values_err)
 
-  for x in [0, 0.0, "0", "0.5"]:
-    assert my_floor(x)==math.floor(x), f"my_floor failed for input {x}"
+def test_my_floor(test_values):
+  """Mock unit test which runs on a set of test values from a flexible fixture.
+  """
+  _test_my_floor(test_values)
+
+@pytest.mark.skip
+def test_my_floor_skip():
+  """Mock unit test which is skipped
+  """
+  pass
