b5fe88badc46aa5620e45bfd701175fce1200b7c
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2024-06-25 16:29:06 +0100

    Output test results from JSON to widget

--- run_tests.py
+++ run_tests.py
@@ -7,8 +7,10 @@
 Entry-point file to run the interface and unit tests
 """
 
+import json
 import os
 import sys
+import tempfile
 
 import subprocess
 from PySide6 import QtCore, QtWidgets
@@ -19,13 +21,17 @@ class MyWidget(QtWidgets.QWidget):
 
     self.test_module = test_module
 
-    self.button = QtWidgets.QPushButton("Click me!")
+    self.layout: QtWidgets.QVBoxLayout = QtWidgets.QVBoxLayout(self)
+
     self.text = QtWidgets.QLabel("Press the button to run the unit tests!",
                                  alignment=QtCore.Qt.AlignCenter)
-    
-    self.layout: QtWidgets.QVBoxLayout = QtWidgets.QVBoxLayout(self)
     self.layout.addWidget(self.text)
+
+    self.button = QtWidgets.QPushButton("Click me!")
     self.layout.addWidget(self.button)
+
+    self.results_visible = False
+    self.results_label = QtWidgets.QLabel("", alignment=QtCore.Qt.AlignLeft)
 
     self.button.clicked.connect(self.run_tests)
 
@@ -35,8 +41,18 @@ class MyWidget(QtWidgets.QWidget):
     # TODO - Replace the next bit with use of QT to make an interface to get values and call pytest with them
     test_values = [0, 2, 4, 1.5, -1.5]
 
-    # Call pytest on the test module
-    subprocess.call([sys.executable, "-m", "pytest", self.test_module, "--test_values", *map(str,test_values)])
+    # Create a temporary JSON file for output
+    tmp_json = tempfile.NamedTemporaryFile(delete=False)
+    try:
+      # Call pytest on the test module
+      subprocess.run([sys.executable, "-m", "pytest", f"--{tmp_json.name}", self.test_module, "--test_values",
+                      *map(str,test_values)], capture_output=True)
+      import pdb; pdb.set_trace()
+      d_results = json.load(open(tmp_json.name,'r'))
+      self.results_label.setText("Tests complete!\n" + repr(d_results))
+      self.layout.addWidget(self.results_label)
+    finally:
+        tmp_json.close()
 
 if __name__ == "__main__":
 
