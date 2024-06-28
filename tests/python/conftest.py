f5933a6f25845e0f0826fe1c1424f51a711614dd
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2024-06-28 16:49:24 +0100

    Add docstrings and fill out details

--- tests/python/conftest.py
+++ tests/python/conftest.py
@@ -4,15 +4,21 @@
 :date: 2024/06/25
 :author: Bryan Gillis
 
-pytest test configuration.
+Common code to be run by pytest for all unit tests.
 """
 
 import pytest
 
 def pytest_addoption(parser):
-    parser.addoption("--test_values", nargs="+", default=[0, 1, 1.0, 1.5], type=str)
+  """Function called automatically by pytest to add an option to its command-line invocation. This option allows the
+  user to specify a list of values which will be used for one of the unit tests.
+  """
+  parser.addoption("--test_values", nargs="+", default=[0, 1, 1.0, 1.5], type=str)
 
 
 @pytest.fixture
 def test_values(request):
-    return request.config.getoption("--test_values")
+  """Fixture providing a set of values which can be specified at the invocation of pytest, default to a set which will
+  all pass.
+  """
+  return request.config.getoption("--test_values")
