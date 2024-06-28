6ef579525f867bcf9ccb854228eb3aea8ec5f868
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2024-06-28 16:42:01 +0100

    Fill out docstrings

--- tests/python/test_values.py
+++ tests/python/test_values.py
@@ -4,7 +4,8 @@
 :date: 2024/06/25
 :author: Bryan Gillis
 
-Mock unit tests of functions which fail on certain values.
+Mock implementation and unit tests of a function `my_floor` which is known to fail on certain values (any non-integer
+negative value), to aid in demonstrating the Qt interface for running unit tests.
 """
 
 import math
@@ -12,24 +13,31 @@ import math
 import pytest
 
 def my_floor(x):
-  """Mock implementation of a "floor" function, which fails on negative non-integer values.
+  """Mock implementation of a "floor" function, which fails on any non-integer negative values.
   """
   return int(x)
 
 @pytest.fixture
 def test_values_pass():
+  """Fixture providing a set of values which it is known that the mock function works correctly on.
+  """
   return [0.5, 1.0, 0.0, 1.1, 2.3, 100000, 1e99]
 
 @pytest.fixture
 def test_values_fail():
+  """Fixture providing a set of values, including some which it is known the mock function works incorrectly on.
+  """
   return [0, 0.0, 1.0, 1.2, 1.9, -1.0, -1.5, -1.9, -2]
 
 @pytest.fixture
 def test_values_err():
+  """Fixture providing a set of values, including one which it is known the function will raise an exception for.
+  """
   return [0, 0.0, math.nan]
 
 def _test_my_floor(_test_values):
-  """Common implementation of tests.
+  """Common implementation of all different unit tests. The mock function is run on each value, and the result is
+  compared with the results of the built-in `math.floor` function.
   """
   for x in map(float,_test_values):
     assert my_floor(x)==math.floor(x), f"my_floor failed for input {x}"
@@ -50,12 +58,15 @@ def test_my_floor_err(test_values_err):
   _test_my_floor(test_values_err)
 
 def test_my_floor(test_values):
-  """Mock unit test which runs on a set of test values from a flexible fixture.
+  """Mock unit test which runs on a set of test values from a flexible fixture (defined in conftest.py). The values
+  provided to this fixture can be set by passing a list of values in the invocation of pytest through e.g.
+  `... --test values 0 1 2 3`. If no custom values are provided, a default list of values will be used where the
+  function will pass the test.
   """
   _test_my_floor(test_values)
 
 @pytest.mark.skip
 def test_my_floor_skip():
-  """Mock unit test which is skipped
+  """Mock unit test which is skipped.
   """
   pass
